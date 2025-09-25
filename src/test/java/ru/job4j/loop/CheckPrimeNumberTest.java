dff00905d284f5a897aa7d620388b2e4df32ecfe
ToT707 <easy0894@gmail.com>
2025-09-25 16:02:13 +0300

    Простое число (оператор прерывания break) [#505214]

---
+++ src/test/java/ru/job4j/loop/CheckPrimeNumberTest.java
@@ -0,0 +1,41 @@
+package ru.job4j.loop;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CheckPrimeNumberTest {
+    @Test
+    void whenNumber7ThenTrue() {
+        int number = 7;
+        boolean result = CheckPrimeNumber.check(number);
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void whenNumber13ThenTrue() {
+        int number = 13;
+        boolean result = CheckPrimeNumber.check(number);
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void whenNumber0ThenFalse() {
+        int number = 0;
+        boolean result = CheckPrimeNumber.check(number);
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void whenNumber1ThenFalse() {
+        int number = 1;
+        boolean result = CheckPrimeNumber.check(number);
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void whenNumber27ThenFalse() {
+        int number = 27;
+        boolean result = CheckPrimeNumber.check(number);
+        assertThat(result).isTrue();
+    }
+}
