a96f6b83aa4d741bf6f48706bbaf0184807dc652
ToT707 <easy0894@gmail.com>
2026-01-28 11:43:23 +0300

    2.0. Рефакторинг - Расстояние между точками. [#122587]

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -6,27 +6,31 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.withPrecision;
 
 class PointTest {
+
     @Test
     void whenPoints12And34Then2dot82() {
-        int x1 = 1, y1 = 2, x2 = 3, y2 = 4;
+        Point a = new Point(1, 2);
+        Point b = new Point(3, 4);
         double expected = 2.82;
-        double output = Point.distance(x1, y1, x2, y2);
+        double output = a.distance(b);
         assertThat(output).isEqualTo(expected, withPrecision(0.01));
     }
 
     @Test
     void whenPoints58And26Then3dot6() {
-        int x1 = 5, y1 = 8, x2 = 2, y2 = 6;
+        Point a = new Point(5, 8);
+        Point b = new Point(2, 6);
         double expected = 3.6;
-        double output = Point.distance(x1, y1, x2, y2);
+        double output = a.distance(b);
         assertThat(output).isEqualTo(expected, withPrecision(0.01));
     }
 
     @Test
     void whenPointsMinus12And34Then7dot21() {
-        int x1 = -1, y1 = -2, x2 = 3, y2 = 4;
+        Point a = new Point(-1, -2);
+        Point b = new Point(3, 4);
         double expected = 7.21;
-        double output = Point.distance(x1, y1, x2, y2);
+        double output = a.distance(b);
         assertThat(output).isEqualTo(expected, withPrecision(0.01));
     }
 }
